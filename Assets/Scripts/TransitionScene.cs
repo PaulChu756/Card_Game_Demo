a68296f8d6930802be592246ef1c2dcf3da53b7f
Chui <lordbabypanda@gmail.com>
2016-04-07 12:08:01 -0500

    Transition scenes work

---
+++ Assets/Scripts/TransitionScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace chui
+{
+    public class TransitionScene : MonoBehaviour
+    {
+        public void CombatButton()
+        {
+            LevelLoader.LoadLevel("Combat");
+        }
+
+        public void ExitButton()
+        {
+            LevelLoader.LoadLevel("Exit");
+        }
+    }
+}
